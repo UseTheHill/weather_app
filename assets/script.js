0a3aaa36e3852990fbb8567cf6fbd53e95c6bfe3
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-01 14:32:08 -0500

    js variables double checked linking to the right class name

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//global variables
+let dateClass = $(".date").toArray();
+let tempClass = $(".temp").toArray();
+let humidityClass = $(".humidity").toArray();
+let date = moment().format("L");
+let iconArray = $(".weather_icon").toArray();
+let SearchHistoryBtn = $(".searchBtn").toArray();
+let userSearchArray = [];
+
