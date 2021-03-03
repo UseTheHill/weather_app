7f24955ce978aa5b2b938db7e371a4bf3b8903b4
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-03 12:58:36 -0500

    search btn and saving search history

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,31 @@ let iconArray = $(".weather_icon").toArray();
 let SearchHistoryBtn = $(".searchBtn").toArray();
 let userSearchArray = [];
 
+//search button generates weather
+$(".searchBtn").onclick("click", function() {
+    event.preventDefault();
+    userSearch = $("#userSearch").val();
+
+//add search to search history unless it is already there
+    if ($("#userSearch").val() !== "") {
+        let userSearchP = $("<button>" + userSearch + "</button>");
+        userSearchP.attr("data-city", $("#userSearch").val());
+        userSearchP.addClass("searchHistoryBtn btn btn-outline-primary");
+
+        let newCity = userSearchP.attr("data-city");
+
+        if (userSearchArray.includes(newCity)) {
+        }
+
+        else {
+            $("#search-history").append(userSearchP);
+            userSearchArray.push(newCity);
+            storeSearchHistory();
+        }
+
+    getWeatherData(userSearch);
+    storeLastSearch();
+    $("#userSearch").val("");
+    }
+    
+});
